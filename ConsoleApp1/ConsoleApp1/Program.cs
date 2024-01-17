b77e7f29194657046a6c0cbca017a128b1026166
hareem13 <hareemrehan07@gmail.com>
2024-01-17 19:52:23 +0500

    dfsd

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ques 1
+            BankAccount myAccount = new BankAccount(5000.0,5.0);
+            // Deposit 
+            myAccount.Deposit(500.0);
+
+            // Withdraw 
+            myAccount.Withdrawl(200.0);
+            //Monthly Processing
+            myAccount.MonthlyProcess();
+            Console.WriteLine($"Current Balance:{myAccount.GetBalance()}");
+            Console.WriteLine($"Number Of Deposits:{myAccount.GetNumberOfDeposits()}");
+            Console.WriteLine($"NumberOfWithdrawls:{myAccount.GetNumberOfWithdrawls()}");
+
+
+            //Ques 2
+            Hotel myHotel = new Hotel("ABC Hotel", "123 street", "07854531");
+            Console.WriteLine($"Hotel Name:{myHotel.GetHotelName()}");
+            Console.WriteLine($"Address:{myHotel.GetAddress()}");
+            Console.WriteLine($"Tax Number:{myHotel.GetTaxNumber()}");
+            Console.ReadLine(); 
+        }
+              
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/assignment.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    //Ques 1
+    internal class BankAccount
+    {
+        public double balance;
+        public double monthlyservicecharges;
+        public int numberOfDeposits;
+        public int numberofwithdrawls;
+        //Constructor
+        public BankAccount(double balance, double monthlyservicecharges)
+        {
+            this.balance = balance;
+            this.monthlyservicecharges = monthlyservicecharges;
+            numberOfDeposits = 0;
+            numberofwithdrawls = 0;
+        }
+        //Deposit Method
+        public void Deposit(double depositamount)
+        {
+            balance += depositamount;
+            numberOfDeposits++;
+        }
+        //Withdrawl Method
+        public void Withdrawl(double withdrawlamount) {
+            balance -= withdrawlamount;
+            numberofwithdrawls++;
+        }
+        //MontlyProcess Method
+        public void MonthlyProcess()
+        {
+            balance -= monthlyservicecharges;
+        }
+        //Get Methods
+        public double GetBalance() { 
+        return balance;
+        }
+        public double GetNumberOfDeposits()
+        {
+            return numberOfDeposits;    
+        }
+        public int GetNumberOfWithdrawls()
+        {
+            return numberofwithdrawls;
+        }
+       
+    }
+    // Ques 2
+    internal class Hotel
+    {
+        public string hotel_name;
+        public string address;
+        public string tax_number;
+        //Constructor
+        public Hotel(string hotel_name, string address , string tax_number )
+        {
+            this.hotel_name = hotel_name;
+            this.address = address; 
+            this.tax_number= tax_number;
+
+        }
+        //Methods
+        public void SetHotelName(string name)
+        {
+            hotel_name = name;
+        }
+        public void SetAddress(string addr)
+        {
+            address = addr;
+        }
+        public void SetTaxnumber(string taxnumber)
+        {
+            tax_number = taxnumber; 
+        }
+        //Get Methods
+        public string GetHotelName()
+        {
+            return  hotel_name;
+        }
+        public string GetAddress()
+        {
+            return address;
+        }
+        public string GetTaxNumber()
+        {
+            return tax_number;
+        }
+    }
+}
